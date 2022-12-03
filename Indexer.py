f412ca00cd5ec23a8ba2977d044ee567b64dfe3c
ronm100 <57960026+ronm100@users.noreply.github.com>
2022-12-03 12:25:31 +0200

    hemopolymer filter fix

--- Indexer.py
+++ Indexer.py
@@ -1,18 +1,19 @@
 from itertools import product
+from typing import Tuple
 
-def has_bad_sequence(l: list) -> bool:
-    return ((0,) * 5 in l) or ((1,) * 5 in l) or ((2,) * 5 in l) or ((3,) * 5 in l) 
+HMPLMR_LEN = 5
+
+def has_bad_sequence(l: Tuple) -> bool:
+    l = list(l)
+    for i in range(len(l) - HMPLMR_LEN + 1):
+        if l[i : i+HMPLMR_LEN] == [0] * HMPLMR_LEN or l[i : i+HMPLMR_LEN] == [1] * HMPLMR_LEN or l[i : i+HMPLMR_LEN] == [2] * HMPLMR_LEN or l[i : i+HMPLMR_LEN] == [3] * HMPLMR_LEN:
+            return True
 
 def create_indices(k: int):
     vector_space = [0, 1, 2 ,3]
     all_vectors = product(vector_space, repeat=k)
-
-    # filtered_vectors = filter(has_bad_sequence, all_vectors)
     filtered_vectors = [vec for vec in all_vectors if not has_bad_sequence(vec)]
-    all_vectors_2 = [vec for vec in all_vectors]
-    print(len(list(all_vectors)))
-    print(len(list(filtered_vectors)))
+    return filtered_vectors
 
 if __name__ == '__main__':
-    create_indices(k=5)
-    # print(has_bad_sequence((0,0,0,0,0)))
+    create_indices(k=7)
