cdd9d28c2707924aa34c1556d4221c4dd114c05f
ronm100 <57960026+ronm100@users.noreply.github.com>
2022-12-24 18:47:03 +0200

    get_parity_check_matrix implemented

--- Indexer.py
+++ Indexer.py
@@ -1,6 +1,7 @@
 from itertools import product
 from typing import Tuple
 import numpy as np
+import math
 import galois
 
 HMPLMR_LEN = 5
@@ -16,16 +17,35 @@ def has_bad_sequence(vec: Tuple) -> bool:
 
 
 def get_code_dimensions(data_len: int) -> Tuple[int, int]:
-    for i in range(100):
-        if data_len <= (4 ** i) - 1 - i:
-            return (4 ** i) - 1, (4 ** i) - 1 - i
+    for redundancy_len in range(100):
+        message_len = ((4 ** redundancy_len) - 1) / 3
+        if data_len <= message_len - redundancy_len:
+            return message_len, redundancy_len
     return None, None  # Shouldn't get here
+
+
+def get_parity_check_matrix(message_len, redundancy_len):
+    total_len = 1
+    curr_block_exp = 1
+    vector_space = [0, 1, 2, 3]
+    parity_check_matrix = np.transpose(np.array((redundancy_len - 1) * [0] + [1]))
+    parity_check_matrix = np.expand_dims(parity_check_matrix, axis=1)
+    while total_len < message_len:
+        block = np.zeros((redundancy_len - curr_block_exp - 1, 4 ** curr_block_exp))
+        block = np.concatenate([block, np.ones((1, 4 ** curr_block_exp))], axis=0)
+        vec_combination = [vec for vec in product(vector_space, repeat=curr_block_exp)]
+        block = np.concatenate([block, np.transpose(np.array(vec_combination))], axis=0)
+        parity_check_matrix = np.concatenate([parity_check_matrix, block], axis=1)
+        total_len += 4 ** curr_block_exp
+        curr_block_exp += 1
+    return parity_check_matrix
 
 
 def get_generator_matrix(data_len):
     # returns A matrix
     message_len, total_data_len = get_code_dimensions(data_len)
     parity_len = message_len - total_data_len
+
     non_zero_vecs = product([0, 1], repeat=parity_len)
     A_trans = [vec for vec in non_zero_vecs if sum(vec) > 1]
     A_trans = GF(A_trans)
@@ -47,8 +67,10 @@ def create_indices(k: int):
 
 
 if __name__ == '__main__':
-    GF = galois.GF(4)
-    create_indices(k=13)
+    me, re = get_code_dimensions(18)
+    a = get_parity_check_matrix(me, re)
+    # GF = galois.GF(4)
+    # create_indices(k=13)
     # get_generator_matrix(7,4)
     # x = calc_parity_bits(np.array([1,0,0,0]))
     a = 1
