ac9aa3aec74dbd87bc052f83b50aab5adcbb0bf8
ronm100 <ronm64@gmail.com>
2023-04-04 20:27:43 +0000

    matrix filtering

--- Indexer.py
+++ Indexer.py
@@ -136,44 +136,74 @@ def calc_distances(codes: np.array):
                 pickle.dump(results, f)
             word_tuples = np.zeros(shape=(1, 2*codes.shape[1]),dtype=np.int8)
     print(f'word_tuples.shape = {word_tuples.shape}')
-    dim = len(codes)
-    shape = dim, dim
-    matrix = np.zeros(shape=shape, dtype=np.int8)
-    for dist, i, j in results:
-        matrix[i,j] = dist
-    return matrix
+
 
     time_1 = time.time()
     print(f'tuples took {time_1 - time_0}, starting Pool')
     time_2 = time.time()
     print(f'edlib took {time_2 - time_1}')
 
+def index_codes(codes: np.array):
+    word_to_num = {str(codes[i,:]).replace('[','').replace(',','').replace(']','').replace(' ',''):i for i in range(len(codes))}
+    print('done_1')
+    num_to_word = {i : str(codes[i,:]).replace('[','').replace(',','').replace(']','').replace(' ','') for i in range(len(codes))}
+    print('done_2')
+    with open(f'edit_distances_01/word_to_num.pkl','wb') as f:
+        pickle.dump(word_to_num, f)
+    with open(f'edit_distances_01/num_to_word.pkl','wb') as f:
+        pickle.dump(num_to_word, f)
+
 def calc_edit_dist_matrix(dists_dir):
-    for index, filename in enumerate(os.listdir(dir)):
+    with open(f'edit_distances_01/word_to_num.pkl','rb') as f:
+        word_to_num = pickle.load(f)
+    # with open(f'edit_distances_01/num_to_word.pkl','rb') as f:
+    #     num_to_word = pickle.load(f)
+
+    dim = len(word_to_num)
+    shape = dim, dim
+    matrix = np.zeros(shape=shape, dtype=np.int8)
+    for filename in os.listdir(dists_dir):
+        print(f'start {filename}')
+        if 'dists_' not in filename:
+            continue
+        with open(f'edit_distances_01/{filename}','rb') as f:
+            dists = pickle.load(f)
+        # try:
+        dists = [dist for dist in dists if dist[0] == True]
+        # except TypeError:
+        #     print('passed')
+        #     pass
+        for dist in dists:
+            matrix[word_to_num[dist[1]], word_to_num[dist[2]]] = 1
+    np.save(Path('edit_distances_01') / 'dist_matrix.npy', matrix)
 
 
 
-
-def filter_codes_by_edit_dist(init_code_book, distance_matrix):
-    # while np.any(distance_matrix):
-    #     bad_row_indices = np.where(np.any(distance_matrix > 0, axis=1))
-    bad_row_indices = list(np.where(np.any(distance_matrix > 0, axis=1))[0])
-    if not bad_row_indices:
-        return init_code_book
-
-    # bad_words = [init_code_book[i] for i in list(bad_row_indices)]
-    filtered_code_book = [init_code_book[i] for i in range(len(init_code_book)) if i not in bad_row_indices]
-    return list(filtered_code_book)
+def filter_codes_by_edit_dist(distance_matrix):
+    with open(f'edit_distances_01/num_to_word.pkl','rb') as f:
+        num_to_word = pickle.load(f)
+    while np.any(distance_matrix):
+        # bad_row_indices = np.where(np.any(distance_matrix > 0, axis=1))
+        n_mismatch = np.sum(distance_matrix, axis=1)
+        max_val_idx = np.argmax(n_mismatch)
+        distance_matrix[max_val_idx,:] = np.zeros(shape=distance_matrix.shape[0], dtype=np.int8)
+        distance_matrix[:,max_val_idx] = np.zeros(shape=distance_matrix.shape[0], dtype=np.int8)
+        del num_to_word[max_val_idx]
+    print('done')
+    with open(f'edit_distances_01/final_codes.pkl','wb') as f:
+        pickle.dump(num_to_word, f)
 
 
 if __name__ == '__main__':
     # generate_codes(k=18)
 
-    filtered_code_book = np.load('generated_vectors610_filtered/filtered_vecs_18_1.npy')
-    frac = int(filtered_code_book.shape[0] / 100)
-    filtered_code_book = filtered_code_book[0:frac,:]
-    distance_mat = calc_distances(filtered_code_book)
+    # filtered_code_book = np.load('generated_vectors610_filtered/filtered_vecs_18_1.npy')
+    # frac = int(filtered_code_book.shape[0] / 100)
+    # filtered_code_book = filtered_code_book[0:frac,:]
+    # calc_edit_dist_matrix('edit_distances_01')
 
+    distance_matrix = np.load('edit_distances_01/dist_matrix.npy')
+    filter_codes_by_edit_dist(distance_matrix)
 
     # filtered_code_book = filter_codes_by_edit_dist(unfiltered_code_book, distance_mat)
     # print('done')
